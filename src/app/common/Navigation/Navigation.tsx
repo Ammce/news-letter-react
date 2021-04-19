6f014119771bc77da58a24bf2b51fb858ae89119
Amel Muminovc <amcenp@gmail.com>
2021-04-19 17:17:29 +0200

    navigation fixes

--- src/app/common/Navigation/Navigation.tsx
+++ src/app/common/Navigation/Navigation.tsx
@@ -10,12 +10,13 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
+import ListIcon from '@material-ui/icons/List';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { NavLink, Switch, Route } from 'react-router-dom';
+import { NavLink, Switch, Route, Redirect } from 'react-router-dom';
 
 import { GiveConsent, CollectedConsents } from '../../modules/Consents/';
 
@@ -74,14 +75,14 @@ function ResponsiveDrawer(props: any) {
       <Divider />
       <List>
         <NavLink
-          to='/'
+          to='/give-consent'
           activeStyle={{ color: '#3f51b5' }}
           exact
           className={classes.link}
         >
           <ListItem button key={'Give Consent'}>
             <ListItemIcon>
-              <MailIcon />
+              <AddCircleIcon />
             </ListItemIcon>
             <ListItemText primary={'Give Consent'} />
           </ListItem>
@@ -93,7 +94,7 @@ function ResponsiveDrawer(props: any) {
         >
           <ListItem button key={'Collected Consents'}>
             <ListItemIcon>
-              <MailIcon />
+              <ListIcon />
             </ListItemIcon>
             <ListItemText primary={'Collected Consents'} />
           </ListItem>
@@ -120,12 +121,11 @@ function ResponsiveDrawer(props: any) {
             <MenuIcon />
           </IconButton>
           <Typography variant='h6' noWrap>
-            Responsive drawer
+            Consents
           </Typography>
         </Toolbar>
       </AppBar>
       <nav className={classes.drawer} aria-label='mailbox folders'>
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation='css'>
           <Drawer
             container={container}
@@ -160,7 +160,14 @@ function ResponsiveDrawer(props: any) {
         <Switch>
           <Route
             exact
-            path={`/`}
+            path='/'
+            render={() => {
+              return <Redirect to='/give-consent' />;
+            }}
+          />
+          <Route
+            exact
+            path={`/give-consent`}
             render={(props) => <GiveConsent {...props} />}
           />
           <Route
@@ -168,45 +175,12 @@ function ResponsiveDrawer(props: any) {
             render={(props) => <CollectedConsents {...props} />}
           />
         </Switch>
-        {/* <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography> */}
       </main>
     </div>
   );
 }
 
 ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window: PropTypes.func,
 };
 
